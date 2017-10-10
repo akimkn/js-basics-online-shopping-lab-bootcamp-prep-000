a16a9f4bc23507ff3c3f9172caed8e3ca6e55511
Austin Kim <ask199416@gmail.com>
2017-10-10 22:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var result = {`${item}`: Math.floor((Math.random() * 100)) + 1}
-  cart.push(result)
+  var wishlist = {`${item}`: Math.floor((Math.random() * 100)) + 1}
+  cart.push(wishlist)
   console.log(`${item} has been added to your cart.`)
   return cart
  // write your code here
