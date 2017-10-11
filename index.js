4460c39ad73eb4c52ab46cced7123ab9efd9abc4
Austin Kim <ask199416@gmail.com>
2017-10-11 03:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber !== undefined) {
+  if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   // write your code here
