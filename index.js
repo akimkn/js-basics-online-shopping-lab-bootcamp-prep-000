e5f5992de665227856ddf9ba21424ed8f8c77722
Austin Kim <ask199416@gmail.com>
2017-10-11 04:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addToCart(item) {
   return cart
 }
 
-/*function viewCart (){
+function viewCart (){
 if(cart.length === 0){
  console.log(`Your shopping cart is empty.`)
 }
@@ -24,28 +24,37 @@ else{
      var myString = "In your cart, you have "
        for(var i = 0, l = cart.length; i < l; i++){
             for(var list in cart[i]){
-           myString+=`${list} at $${cart[i][list]}`
-               if(i!==cart.length-1){
-                myString+=", "
-             }
-             else{
-  myString+="."
-}
-}
-}
- console.log(myString)
-}
-}*/
+                myString+=`${list} at $${cart[i][list]}`
+                if(i!==cart.length-1){
+                  myString+=", "
+                  }
+                else{
+                  myString+="."
+                  }
+                }
+              }
+              console.log(myString)
+            }
+          }
+
 function viewCart() {
+  var result = "In your cart, you have "
   if (cart = []) {
     console.log('Your shopping cart is empty.')
   }
   if (cart.length = 1) {
     for (var i = 0; i < 1; i++) {
       for (var element in cart[i]) {
-        console.log(`In your cart, you have ${element} at $${cart[i][element]}.`)
+        result += `${element} at $${cart[i][element]}`
+        if (i !== cart.length-1) {
+          result += ", "
+        }
+        else {
+          result += "."
+        }
       }
     }
+    console.log(result)
   }
 }
 
