3e6ee49df5d3008aa4900a54a7edcd62516c605e
Austin Kim <ask199416@gmail.com>
2017-10-11 03:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,8 +77,9 @@ function placeOrder(cardNumber) {
   var numb = cardNumber;
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
-  } else {
-      console.log(`Your total cost is "$"${total()}, which will be charged to the card ${numb}.`)
-      cart = [];
+  } 
+  if (cardNumber) {
+    console.log("Your total cost is "$"${total()}, which will be charged to the card ${numb}.");
+    cart = [];
   }
 }
