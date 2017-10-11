26e6726145b2c98d95631bafa5548fd47d773ae7
Austin Kim <ask199416@gmail.com>
2017-10-11 01:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ function removeFromCart(item) {
       cart.splice(i,1)
       return cart;
     } else (console.log('That item is not in your cart.'));
+    return cart
   }
   return cart
   // write your code here
