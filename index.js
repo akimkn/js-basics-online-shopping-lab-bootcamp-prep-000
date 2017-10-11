2b29c3bfa28c54e9b783a5a918b3de53438f5504
Austin Kim <ask199416@gmail.com>
2017-10-11 00:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
     console.log('Your shopping cart is empty.')
   }
   if (cart.length = 1) {
-    console.log (`In your cart, you have ${Object.keys(cart)[0]} at ${Object.values(cart)[0]}`)
+    console.log (`In your cart, you have ${cart[item][0]} at ${Object.values(cart)[0]}`)
     }
   // write your code here
 }
