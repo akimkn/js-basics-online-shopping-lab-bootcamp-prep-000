b4cd1b778870ab90568ff0dea62891ac2721e09c
Austin Kim <ask199416@gmail.com>
2017-10-11 04:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
-  else if (cardNumber) {
+  else {
     console.log(`Your total cost is "$"${total()}, which will be charged to the card ${cardNumber}`)
     cart = [];
   }
