f16b87cf9368bcd29cefdd990d43da34ae1591dc
Austin Kim <ask199416@gmail.com>
2017-10-11 01:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 function removeFromCart(item) {
   for (var i=0; i<cart.length; i++) {
     if (cart[i].hasOwnProperty(`${item}`)) {
-      cart = cart.splice(i,1)
+      cart.splice(i,1)
       break;
     }
   }
