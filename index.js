01e0b1fb87c9fd8b10aa176d13ff770a92b95511
Austin Kim <ask199416@gmail.com>
2017-10-11 03:38:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var numb = cardNumber;
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
